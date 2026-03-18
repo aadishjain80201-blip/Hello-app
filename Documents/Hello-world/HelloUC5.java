0510f362b0b86cba72c01b9be637c355e7f5ebaa
Your Name <youremail@gmail.com>
2026-03-18 08:43:43 +0530

    Multiple Command-Line Arguments using Enhanced For Loop or Default Message

---
+++ Documents/Hello-world/HelloUC5.java
@@ -0,0 +1,14 @@
+public class HelloUC5 {
+    public static void main(String[] args) {
+
+        // If no command-line arguments, print default message
+        if (args.length == 0) {
+            System.out.println("Hello");
+        } else {
+            // Use enhanced for loop to print Hello with each argument
+            for (String arg : args) {
+                System.out.println("Hello " + arg);
+            }
+        }
+    }
+}
